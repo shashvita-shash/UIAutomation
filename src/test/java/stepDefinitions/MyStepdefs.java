d8d10ebc0d4386cf6eba622aa12fb9a730cde6a5
shashvita-shash <sasvitha1999@gmail.com>
2026-07-16 20:24:46 +0530

    first commit

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -34,6 +34,7 @@ public class MyStepdefs {
         System.out.println(attribute);
         act.click(driver.findElement(By.xpath("//a[@title='Active Link']"))).perform();
         driver.quit();
+        driver.close();
 
     }
 }
